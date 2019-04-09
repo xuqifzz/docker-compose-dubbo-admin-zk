cdc7efca19ee33ac013b2b79912abd39a0db04b2
xuqi <65060782@qq.com>
2019-04-10 00:29:26 +0800

    修正docker没启动的问题

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
+systemctl start docker
 # 安装docker-compose
 
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
