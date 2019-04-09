dcd0948161511e1ffe28b4186555aa52ae323a64
xuqi <65060782@qq.com>
2019-04-10 00:11:35 +0800

    第一次提交

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM openjdk:8-jdk-alpine
+
+LABEL maintainer="mail@xuqi.io"
+
+VOLUME /tmp
+
+ADD ./dubbo-admin-0.0.1-SNAPSHOT.jar app.jar
+
+ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+
---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 安装docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# 安装docker-compose
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# 制作镜像
+docker build -t dubbo-admin:1.0 .
+
+# 启动docker-compose 
+docker-compose up
+
